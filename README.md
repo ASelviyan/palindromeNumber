8003530101259abd62708b1c3906335b997c6530
ASelviyan <angelikaselviyan5@gmail.com>
2022-09-01 19:12:26 -0600

    problem

---
+++ README.md
@@ -0,0 +1,5 @@
+## LEETCODE PROBLEM: PALINDROME NUMBER 
+
+### PROBLEM
+Given an integer x, return true if x is palindrome integer.
+An integer is a palindrome when it reads the same backward as forward.
---
+++ solution.js
@@ -0,0 +1,13 @@
+//PROBLEM
+// Given a integer you'll have to figure out if the number is a palindrome number
+// you'll know that its a Palindrome number if the number is the same if its read the same forwards and backwards 
+
+//if number is smaller then 0 return false 
+// number will be made into a string
+// make the number that is given into an array 
+// The number will me push out of the array and be put into a variable
+//after the whole loop is finished check the reverse variable to the variable that was given to see if they are the same and then return true 
+//else they don't match return false
+
+//CONSTRAINTS
+//the number that is given to us is more then or equal to -2^31 and is smaller then adn equal to 2^31 - 1
