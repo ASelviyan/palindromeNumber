4bcb61d709d900f5faccb3ccec77daa7b1ed2990
ASelviyan <angelikaselviyan5@gmail.com>
2022-09-01 19:15:26 -0600

    solved

--- solution.js
+++ solution.js
@@ -11,3 +11,28 @@
 
 //CONSTRAINTS
 //the number that is given to us is more then or equal to -2^31 and is smaller then adn equal to 2^31 - 1
+
+var isPalindrome = function (x) {
+    //if number is smaller then 0 return false 
+    if (x < 0) {
+        return false
+    }
+    let reverseNum = ''
+    // number will be made into a string
+    const stringX = x.toString()
+
+    // make the number that is given into an array 
+    const arrX = stringX.split("")
+
+    // The number will me push out of the array and be put into a varaible
+    const reverseArray = arrX.reverse()
+    reverseNum = reverseArray.join("")
+
+    //after the whole loop is finished check the reverse varaible to the variable that was given to see if they are the same and then return true 
+    if (x === Number(reverseNum)) {
+        return true
+        //else they dont match return false
+    } else {
+        return false
+    }
+};
